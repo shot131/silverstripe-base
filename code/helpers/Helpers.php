def877f487771a63410e7868dd1d5df2ceec1de8
nikita <nikita@unicontent.ru>
2018-04-20 18:03:52 +0300

    follow redirects while downloading a file

--- code/helpers/Helpers.php
+++ code/helpers/Helpers.php
@@ -111,6 +111,7 @@ class Helpers
             curl_setopt($ch, CURLOPT_POST, 0);
             curl_setopt($ch, CURLOPT_URL, $src);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
             curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
             $st = curl_exec($ch);
             if ($st === false) return null;
